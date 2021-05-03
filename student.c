9377b576702b05a15a6c2c5e01f48140749dc804
JH <22000617@handong.edu>
2021-05-04 06:01:57 +0900

    make functions

---
+++ student.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "student.h"
+
+int updateStudent(Attendance *s){
+	char rename[100];
+	getchar();
+	printf("수정할 학생 이름은?");
+	return 1;
+}
