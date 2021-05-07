6ea4e3cbf6be7c44e20041c68a90b1d21fc463fb
JH <22000617@handong.edu>
2021-05-08 00:53:44 +0900

    Update student.c

--- student.c
+++ student.c
@@ -12,20 +12,20 @@ int selectMenu(){
         printf("3. 학생 정보 수정\n");
         printf("4. 출석 정보 업데이트\n");
         printf("5. 학생 정보 삭제\n");
-        printf("6. 파일 저장\n ");
+        printf("6. 파일 저장\n");
         printf("7. 학생명으로 검색\n");
         printf("8. 학번으로 검색\n");
-        printf("9. 출석횟수로 검색");
+        printf("9. 출석횟수로 검색\n");
         printf("0. 종료\n");
-        printf("원하는 일을 선택해주세요.\n");
+        printf("원하는 일을 선택해주세요: ");
         scanf("%d", &menu);
         return menu;
 } //메뉴 선택 함수
 
 void listStudent(Attendance *s, int count){
    int i = 0;
-    printf("*****************\n");
-    printf("  학번 이름 출석  지각\n");
+    printf("******************************\n");
+    printf("  학번 이름     출석    지각\n");
     for (i = 0; i < count; i++)
     {
         if (s[i].studentno <=0)
@@ -38,7 +38,7 @@ void listStudent(Attendance *s, int count){
 int createStudent(Attendance *s){
     char pname[100];
     getchar();
-    printf("학생명은? ");
+    printf("\n학생명은? ");
     fgets(pname, 100, stdin);
     strcpy(s->name, pname);
     rewind(stdin);
@@ -52,11 +52,11 @@ int createStudent(Attendance *s){
 int updateStudent(Attendance *s){
         char rename[100];
         getchar();
-        printf("수정할 학생 이름은?");
+        printf("\n수정할 학생 이름은? ");
         fgets(rename, 100, stdin);
         rewind(stdin);
         strcpy(s->name, rename);
-        printf("수정할 학번은?");
+        printf("수정할 학번은? ");
         scanf("%d", &s->studentno);
         printf("=> 수정성공!\n");
         return 1;
@@ -64,16 +64,12 @@ int updateStudent(Attendance *s){
 } // 학생 정보 수정 : 메뉴 3번
 
 int updateAttendance(Attendance *s){
-        int no;
         int up;
 
-        printf("\n 출석? (Yes:1, No:0)");
+        printf("\n출석? (Yes:1, No:0): ");
         scanf("%d", &up);
-        s[no-1].attendance=s[no-1].attendance+1;
-
-        printf("\n 지각? (Yes:1, No:0");
-        scanf("%d", &up);
-        s[no-1].late=s[no-1].late+1;
+	if(up == 1) s->attendance=s->attendance+1;
+	else s->late=s->late+1;
         printf("=> 출석 확인 완료!\n");
         return 1;
 } //출석 확인 : 메뉴 4번
@@ -90,14 +86,15 @@ int deleteStudent(Attendance *s){
 int selectDataNo(Attendance *s, int count){
         int no;
         listStudent(s, count);
-        printf("\n번호는 (취소 :0)?");
+        printf("\n번호는 (취소 :0)? ");
         scanf("%d", &no);
         return no;
 } // 학생 정보 선택 함수
 
 
 void readStudent(Attendance s){
-        printf("%10d %10s %10d(회)  %d(회)\n", s.studentno, s.name, s.attendance, s.late);
+        printf("%d %s %5d(회) %d(회)\n", s.studentno, s.name, s.attendance, s.late);
 }
 // 학생 정보 출력
+
 
